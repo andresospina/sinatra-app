157759a6a1211b143456b4410eb92fd41eea2ce4
Andres Ospina <andresospina@Andress-MacBook-Pro.local>
2018-04-10 19:25:04 -0500

    Ya puede saludarme mi compu

---
+++ solucion.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do
+	# if params[:nombre] != "" && !params[:nombre].nil?
+	 if params[:nombre] != "" && params.has_key?(:nombre)
+		"<h1>Hola #{params[:nombre]}!</h1>"
+	else
+		"<h1>Hola desconocido!</h1>"
+	end
+end
